facffc3f5f5bd332a9a2a508b8c5e3f1c83e48a5
cnSMZ <128873665+cnSMZ@users.noreply.github.com>
2023-04-06 02:29:13 +0800

    Add files via upload

---
+++ MyBase001_03_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBase001_03_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ctrl+Shift+B 生产解决方案 排查语法错误
+
+            Console.WriteLine("Hello World!");//代码打印到控制台 运行:点击启动 F5
+            Console.ReadKey();//暂停 等待任意键继续
+        }
+    }
+}
