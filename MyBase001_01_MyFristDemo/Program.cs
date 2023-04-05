e39d794dde5caec515fc47ab55c9ee7cd37fffc3
cnSMZ <128873665+cnSMZ@users.noreply.github.com>
2023-04-06 02:28:36 +0800

    Add files via upload

---
+++ MyBase001_01_MyFristDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;//命名空间
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBase001_01_MyFristDemo   //项目名称
+{
+    internal class Program   //类
+    {
+        static void Main(string[] args)   //main函数  主入口
+        {
+
+        }
+    }
+}
+
---
+++ MyBase001_02_MySecondDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBase001_02_MySecondDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
