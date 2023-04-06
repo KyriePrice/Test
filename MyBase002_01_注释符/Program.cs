577e3488fd0e9bb4e970656f9ae10a1ebd3f8d9e
cnSMZ <128873665+cnSMZ@users.noreply.github.com>
2023-04-06 21:07:02 +0800

    Add files via upload

---
+++ MyBase002_01_注释符/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBase002_01_注释符
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 文档注释 一般注释类或函数   摘要
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //输出HelloWorld -- 解释作用
+            Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Test!");   注销当前代码 -- 注销作用
+            //暂停当前程序
+
+            Console.ReadKey();
+
+            //   当行注释
+            /* 多行注释 */
+
+        }
+    }
+}
